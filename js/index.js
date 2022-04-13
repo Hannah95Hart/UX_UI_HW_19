89bab6aa8dd0d71f218fc96008c7418ae626894f
Hannah95Hart <92270706+Hannah95Hart@users.noreply.github.com>
2022-04-12 19:25:30 -0600

    Basic jQuiry
    Things are not going well. I can't get any of this to work right

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(".inline-block").on("click", function(){
+    $("#projects").css("color","8C6658");
+$(".inline-block").off("click");
+});
+
+$(".inline-block").on("click", function(){
+    $("#aboutMe").css("color","#8C6658");
+$(".inline-block").off("click");
+});
+
+$(".inline-block").on("click", function(){
+    $("#contact").css("color","#8C6658");
+$(".inline-block").off("click");
+});
