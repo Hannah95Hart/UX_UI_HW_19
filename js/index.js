137221aeee0984cca1e4f3a861e6cc9c230d7253
Hannah95Hart <92270706+Hannah95Hart@users.noreply.github.com>
2022-04-13 13:22:33 -0600

    js update
    I have close to completed the jquiry script, I was able to include a nav bar that moved based on the hover, buttons that are turned on and off to indicate highlight, and a resume button that pulls up a notification

--- js/index.js
+++ js/index.js
@@ -12,3 +12,20 @@ $( "li.fade" ).hover(function() {
   $( this ).fadeOut( 100 );
   $( this ).fadeIn( 500 );
 });
+
+$( "#resume" ).click(function() {
+  alert( "My Resume Would Go Here If I could Figure out How to link it." );
+});
+
+clicked = true;
+$('#projectThree').ready(function(){
+    $("button").click(function(){
+        if(clicked){
+            $(this).css('background-color', '#8C6658');
+            clicked  = false;
+        } else {
+            $(this).css('background-color', '#373738');
+            clicked  = true;
+        }   
+    });
+});
